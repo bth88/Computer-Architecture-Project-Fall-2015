9002917d1f07b7e08211a2f8e97c864b2ac847e8
Leress <jdw751@msstate.edu>
2015-11-24 20:13:40 -0600

    ALU and Mux v2
    changed alu and mux

--- mux.cpp
+++ mux.cpp
@@ -22,9 +22,6 @@ int muxControl(int select2, int select1){
 	else if ((select2 == 1) && (select1 == 0))//10
 		return mux.src2;
 	
-	else if ((select2 == 1) && (select1 == 1))//11
+	else  ((select2 == 1) && (select1 == 1))//11
 		return mux.src3;
-
-	else
-		return 0;// remove laters
 }
